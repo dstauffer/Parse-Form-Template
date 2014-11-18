74e89effd6c2a283c84180819b43ac0c46ef1d13
Daniel Stauffer <dstauffer@tonicdesign.com>
2014-11-18 17:09:53 -0500

    added code for mailgun emailing

--- cloud-code/cloud/main.js
+++ cloud-code/cloud/main.js
@@ -1,6 +1,23 @@
+var Mailgun = require('mailgun');
+//ENTER IN MAILGUN KEYBELOW
+// Mailgun.initialize('tonicdesign.com', 'key--------');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+// Parse.Cloud.beforeSave("SweetObject", function(request, response) {
+//   var contactText = "New Mailgun Email From:\n" +
+//     "Full Name: " + request.object.get("name") + "\n";
+
+//   Mailgun.sendEmail({
+//       to: "stauffer6638@gmail.com",
+//       from: "stauffer6638@gmail.com",
+//       subject: "mailgun Email",
+//       text: contactText
+//     }, {
+//     success: function(httpResponse) {
+//       response.success();
+//     },
+//     error: function(httpResponse) {
+//       console.error(httpResponse);
+//       response.error("Uh oh, something went wrong", httpResponse);
+//     }
+//   });
 });
